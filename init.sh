ccaeffc28d3179a087ae99fd1a0ac6624097867f
EC2 Default User <ec2-user@ip-172-31-27-12.ap-northeast-1.compute.internal>
2018-06-24 09:05:33 +0000

    fail countテーブルついか

--- init.sh
+++ init.sh
@@ -16,3 +16,27 @@ mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} << EOS
 ALTER TABLE login_log ADD INDEX ip(ip);
 ALTER TABLE login_log ADD INDEX user_id(user_id);
 EOS
+
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} << EOS
+CREATE TABLE fail_count (
+  `id`      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` INT,
+  `fail`    INT          DEFAULT 0
+);
+  
+INSERT INTO fail_count(user_id, fail)
+  SELECT
+    ll.user_id,
+    count(1) AS fail
+  FROM login_log ll
+    LEFT JOIN
+    (SELECT
+       user_id,
+       MAX(id) AS last_succeed
+     FROM
+       login_log
+     WHERE succeeded = 1
+     GROUP BY user_id) ls ON ll.user_id = ls.user_id
+  WHERE id > IFNULL(ls.last_succeed, 10000000000)
+  GROUP BY ll.user_id;
+EOS
