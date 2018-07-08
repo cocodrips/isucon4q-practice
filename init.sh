3fa33afa9631abd5af16115be80aa3d408245b08
EC2 Default User <ec2-user@ip-172-31-27-12.ap-northeast-1.compute.internal>
2018-07-08 08:03:53 +0000

    fix init.sh

--- init.sh
+++ init.sh
@@ -3,14 +3,14 @@ set -x
 set -e
 cd $(dirname $0)
 
-myuser=root
-mydb=isu4_qualifier
-myhost=127.0.0.1
-myport=3306
-mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < webapp/initialize.sql
+# myuser=root
+# mydb=isu4_qualifier
+# myhost=127.0.0.1
+# myport=3306
+# mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
+# mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
+# mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
+# mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
+# mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < webapp/initialize.sql
 
-sudo cp dump.rdb /var/lib/redis/dump.rdb
+cp /var/webapp/dump.rdb /var/lib/redis/dump.rdb
